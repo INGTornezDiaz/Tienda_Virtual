15e4cc4a1101b234ce6f10f00f20ae81e47684bf
INGTornezDiaz <apolinartornezdiaz47@gmail.com>
2025-08-26 11:42:22 -0600

    Creación del login

---
+++ Views/dasboard.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title><?php echo $data['tag_page'] ?></title>
+</head>
+<body>
+	<section id="<?php echo $data['page_id'];?>">
+	<h1><?php echo $data['page_title'] ?></h1>
+	<p><?php echo $data['page_content'] ?></p>
+	</section>
+
+</body>
+</html>
--- Views/home.php
+++ Views/home.php
@@ -1,15 +1,26 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title><?php echo $data['tag_page'] ?></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iniciar Sesión | BanyanTreeAccess</title>
+    <link rel="stylesheet" href="./Styles/styles_login.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
-	<section id="<?php echo $data['page_id'];?>">
-	<h1><?php echo $data['page_title'] ?></h1>
-	<p><?php echo $data['page_content'] ?></p>
-	</section>
-
+    <div class="login-container">
+        <div class="logo">
+            <img src="./img/logo_banyan_tree.webp" alt="Logo de Banyan Tree Access">
+        </div>
+        <h1>Bienvenido a BanyanTreeAccess</h1>
+        <h2>Iniciar Sesión</h2>
+        <form action="home.php" method="post" autocomplete="on" novalidate>
+            <input type="text" name="username" placeholder="Usuario" required autofocus>
+            <input type="password" name="password" placeholder="Contraseña" required>
+            <button type="submit">Entrar</button>
+        </form>
+    </div>
 </body>
 </html>
+    
